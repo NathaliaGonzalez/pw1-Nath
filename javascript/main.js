c0c8f85f43baf0f16843e530993596f55943ff8f
nathalia <nrgonzalezd@fctunca.edu.py>
2022-04-06 16:38:24 -0400

    drag

--- javascript/main.js
+++ javascript/main.js
@@ -21,3 +21,20 @@ function makeNormal() {
     var myVideo = document.getElementById("video1");
     myVideo.width = 420; 
 } 
+
+
+
+function getLocation() {
+  var x = document.getElementById("demo");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  var x = document.getElementById("demo");
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
